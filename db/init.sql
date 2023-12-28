639153ec488f078f75a14effc3601402eb185584
blaicheo <78671393+blacheo@users.noreply.github.com>
2023-12-27 22:32:45 -0500

    added sql initialization

---
+++ db/init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE tweets (
+    id int PRIMARY KEY,
+    user_id varchar(30) FOREIGN KEY,
+    content varchar(100)
+);
+
+CREATE TABLE users (
+    username varchar(100)
+);
+
+INSERT INTO users (username)
+VALUES
+('b');
+
+INSERT INTO tweets (id, user_id, content)
+VALUES
+(1, 'b', 'First Tweet'),
+(2, 'b', 'Hello World!');
+
