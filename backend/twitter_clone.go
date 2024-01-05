a58e01713c293071b528179b2f6a4f2855112204
blaicheo <78671393+blacheo@users.noreply.github.com>
2024-01-05 11:09:24 -0500

    fixed backend for docker

--- backend/twitter_clone.go
+++ backend/twitter_clone.go
@@ -1,39 +1,54 @@
 package main
 
 import (
-	"context"
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Server struct {
-	db *pgxpool.Pool
+	db *sql.DB
 }
 
 func main() {
-	conn, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	UNAMEDB := os.Getenv("UNAMEDB")
+	PASSDB := os.Getenv("PASSDB")
+	HOSTDB := os.Getenv("HOSTDB")
+	DBNAME := os.Getenv("DBNAME")
+	os.Getenv("")
+	connStr := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", UNAMEDB, PASSDB, HOSTDB, DBNAME)
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 	}
 
-	defer conn.Close()
+	server := Server{db}
+	router := setupRouter(&server)
+	log.Println("Starting Server")
+	router.Run(":5000")
 
-	server := Server{conn}
+}
+
+func setupRouter(server *Server) *gin.Engine {
 	router := gin.Default()
 	router.GET("/tweet/:id", server.getTweet)
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
 
-	router.Run("localhost:5000")
-
+	return router
 }
 
 func (s *Server) getTweet(c *gin.Context) {
 	id := c.Query("id")
 	var tweet Tweet
-	err := s.db.QueryRow(context.Background(), fmt.Sprintf("SELECT * FROM tweets WHERE id=%s", id)).Scan(&tweet)
+	err := s.db.QueryRow(fmt.Sprintf("SELECT * FROM tweets WHERE id=%s", id)).Scan(&tweet)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
@@ -48,7 +63,7 @@ func postTweet(c *gin.Context) {
 
 type Tweet struct {
 	ID      int    `json:"id"`
-	USER_ID int    `json:"user_id"`
+	USER_ID string `json:"user_id"`
 	Content string `json:"content"`
 }
 
